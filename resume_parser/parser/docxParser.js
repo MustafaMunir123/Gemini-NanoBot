5f2dcb16ee6d92fe1cce47a5160483e1e77a509c
Mustafa Munir <mustafamunir10@gmail.com>
2025-10-22 19:25:43 +0500

    refactor: cleanup and removal of extra files. change dir structure

--- resume_parser/parser/docxParser.js
+++
@@ -1,8 +0,0 @@
-export async function parseDOCX(file) {
-  const mammoth = window.mammoth;
-  if (!mammoth) throw new Error("Mammoth not loaded");
-
-  const arrayBuffer = await file.arrayBuffer();
-  const result = await mammoth.extractRawText({ arrayBuffer });
-  return result.value.trim();
-}
